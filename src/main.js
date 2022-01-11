bdd2dc7cd3eb46137263d533a0bb90f2570918b3
Jason B. Standing <jason.standing@wcbs.co.uk>
2022-01-11 00:26:41 +0000

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store/store.js";
+import vuetify from '@/plugins/vuetify';
 
 Vue.config.productionTip = false;
 
@@ -9,4 +10,5 @@ new Vue({
   router,
   store,
   render: (h) => h(App),
+  vuetify,
 }).$mount("#app");
