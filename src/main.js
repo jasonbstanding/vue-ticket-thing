44d7635866397166f1f7445337df73ad1b5e028f
Jason B. Standing <boondoggle@gimcrack.org>
2024-06-01 23:50:56 +0100

    Spinner

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue';
 import router from './router';
 import VueGtag from "vue-gtag";
 
-createApp(App).use(router).use(VueGtag, {
-    config: { id: "G-7KV1ZJGKK1" }
-  }).mount('#app');
+createApp(App)
+    .use(router)
+    .use(VueGtag, {
+      config: { id: "G-7KV1ZJGKK1" }
+    })
+    .mount('#app');
