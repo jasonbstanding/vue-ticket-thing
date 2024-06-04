aa5d8e7dd94ce5fcee099b340fb7ae0a44ff661f
Jason B. Standing <boondoggle@gimcrack.org>
2024-06-04 23:36:57 +0100

    Queryparams and refactor to composition

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,8 @@
 </template>
 
 <script>
+import { ref, computed, onMounted, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 import axios from 'axios';
 import FiltersComponent from './components/FiltersComponent.vue';
 import MenusComponent from './components/MenusComponent.vue';
@@ -24,7 +26,6 @@ import GigList from './views/GigList.vue';
 import ModalComponent from './components/ModalComponent.vue';
 import Spinner from './components/SpinnerComponent.vue';
 import '@coreui/coreui/dist/css/coreui.min.css';
-import { useRoute } from 'vue-router';
 
 export default {
   name: 'App',
@@ -36,27 +37,28 @@ export default {
     ModalComponent,
     Spinner
   },
-  data() {
-    return {
-      gigs: [],
-      filters: {
-        artist: null,
-        venue: null,
-        gigtype: null,
-        date: null
-      },
-      selectedGig: null,
-      loading: false,
-      artistCounts: {},
-      venueCounts: {},
-      yearCounts: {},
-    };
-  },
-  computed: {
-    filteredGigs() {
-      let filtered = this.gigs;
+  setup() {
+    const route = useRoute();
+    const router = useRouter();
 
-      for (const [key, value] of Object.entries(this.filters)) {
+    const gigs = ref([]);
+    const filters = ref({
+      artist: null,
+      venue: null,
+      gigtype: null,
+      date: null
+    });
+    const selectedGig = ref(null);
+    const loading = ref(false);
+    const artistCounts = ref([]);
+    const venueCounts = ref([]);
+    const yearCounts = ref([]);
+
+    const items = Array.from({ length: 20 }, (_, i) => `Item ${i + 1}`);
+
+    const filteredGigs = computed(() => {
+      let filtered = gigs.value;
+      for (const [key, value] of Object.entries(filters.value)) {
         if (value) {
           filtered = filtered.filter(gig => {
             if (key === 'date') {
@@ -68,98 +70,105 @@ export default {
           });
         }
       }
-
       return filtered;
-    }
-  },
-  methods: {
-    async fetchGigs() {
-      this.loading = true;
+    });
+
+    const fetchGigs = async () => {
+      loading.value = true;
       try {
         const response = await axios.get(process.env.VUE_APP_API_ENDPOINT);
-        this.gigs = response.data;
-        this.processCounts();
+        gigs.value = response.data;
+        processCounts();
+        applyQueryParameters();
       } catch (error) {
         console.error('Error fetching gigs data:', error);
       } finally {
-        this.loading = false;
+        loading.value = false;
       }
-    },
-    processCounts() {
-      const artistMap = {};
-      const venueMap = {};
-      const yearMap = {};
+    };
 
-      this.gigs.forEach(gig => {
+    const processCounts = () => {
+      gigs.value.forEach(gig => {
         // Count artists
         if (gig.artist) {
-          artistMap[gig.artist[0].name] = (artistMap[gig.artist[0].name] || 0) + 1;
+          artistCounts.value[gig.artist[0].name] = (artistCounts.value[gig.artist[0].name] || 0) + 1;
         }
 
         // Count venues
         if (gig.venue) {
-          venueMap[gig.venue[0].name] = (venueMap[gig.venue[0].name] || 0) + 1;
+          venueCounts.value[gig.venue[0].name] = (venueCounts.value[gig.venue[0].name] || 0) + 1;
         }
 
         // Count years
         if (gig.date) {
           const year = gig.date.split('-')[0]; // Extract year from date
-          yearMap[year] = (yearMap[year] || 0) + 1;
+          yearCounts.value[year] = (yearCounts.value[year] || 0) + 1;
         }
       });
+    }
 
-      // Convert back to an array of objects if needed
-      this.artistCounts = artistMap;
-      this.venueCounts = venueMap;
-      this.yearCounts = yearMap;
-    },
-    clearAllFilters() {
-      this.filters = {
+    const applyQueryParameters = () => {
+      const { artist, venue, gigtype, date } = route.query;
+      filters.value.artist = artist || null;
+      filters.value.venue = venue || null;
+      filters.value.gigtype = gigtype || null;
+      filters.value.date = date || null;
+    };
+
+    const updateQueryParameters = () => {
+      const query = { ...filters.value };
+      Object.keys(query).forEach(key => {
+        if (!query[key]) {
+          delete query[key];
+        }
+      });
+      router.push({ query });
+    };
+    
+    watch(() => route.query, applyQueryParameters, { immediate: true });
+
+    onMounted(fetchGigs);
+
+    const clearAllFilters = () => {
+      filters.value = {
         artist: null,
         venue: null,
         gigtype: null,
         date: null
       };
-    },
-    removeFilter(key) {
-      this.filters[key] = null;
-      this.setQuery();
-    },
-    applyFilter(filter) {
-      this.filters[filter.type] = filter.value;
-      this.setQuery();
-    },
-    selectGig(gig) {
-      this.selectedGig = gig;
-    },
-    setQuery() {
-      const query = {}
-      Object.entries(this.filters).forEach(([key, value]) => {
-          if (value) {
-              query[key] = value
-          }
-      })
-      this.$router.push({ query })
-    },
-    async readFilters() {
-      const route = useRoute();
-      if (route.query.artist) {
-        this.applyFilter({ type: "artist", value: route.query.artist});
-      }
-      if (route.query.venue) {
-        this.filters["venue"] = route.query.venue;
-      }
-      if (route.query.date) {
-        this.filters["date"] = route.query.date;
-      }
-    }
-  },
-  created() {
-    this.fetchGigs();
-  },
-  mounted() {
-    this.readFilters();
-  }  
+      updateQueryParameters();
+    };
+
+    const removeFilter = (key) => {
+      filters.value[key] = null;
+      updateQueryParameters();
+    };
+
+    const applyFilter = (filter) => {
+      filters.value[filter.type] = filter.value;
+      updateQueryParameters();
+    };
+
+    const selectGig = (gig) => {
+      selectedGig.value = gig;
+    };
+
+    return {
+      gigs,
+      filters,
+      filteredGigs,
+      selectedGig,
+      loading,
+      clearAllFilters,
+      removeFilter,
+      applyFilter,
+      selectGig,
+      items,
+      artistCounts,
+      venueCounts,
+      yearCounts
+    };
+  }
 };
 </script>
 
