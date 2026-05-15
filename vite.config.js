e6ffdd7e0ed510210ebfb1e076ed9a3b0fc2cbe5
Jason B. Standing <jason@jasonbstanding.com>
2026-05-15 12:17:33 +0100

    Vite config tweak

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: process.env.VITE_BASE_URL || '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // This splits all third-party libraries into a separate 'vendor' chunk
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 })
